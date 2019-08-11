fa7c15cc64a31a8ceebef3e25641a7d7717ed2ae
ose <leslieokoduwa@gmail.com>
2019-08-12 00:15:05 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Palette from './Palette'
+
+class App extends React.Component {
+  render() {
+    return <Palette />;
+  }
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Palette extends React.Component {
+  render () {
+    return (
+      <div>Palette</div>
+    )
+  }
+}
+
+export default Palette
