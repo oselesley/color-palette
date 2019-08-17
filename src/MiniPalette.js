a1967a346077214b2071f70f69a61cd4b3717358
ose <leslieokoduwa@gmail.com>
2019-08-17 19:07:57 +0100

    changed styles to JSS

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,6 +1,44 @@
-import './MiniPalette.css'
+// import './MiniPalette.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/styles'
+
+const styles = {
+  root: {
+    backgroundColor: '#fff',
+    padding: '0.3rem',
+    paddingBottom: '0',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    overflow: 'hidden'
+  },
+  data: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '0 0.2rem',
+    '& span': {
+      // alignSelf: 'flex-start',
+      fontSize: '0.9rem'
+    },
+    '& h3': {
+      /* color: #f4f4f4 */
+      fontSize: '0.7rem',
+      fontFamily: 'Quattrocento Sans, sans-serif',
+      letterSpacing: '1px',
+      alignSelf: 'flex-end',
+      margin: '0 !important',
+      padding: '0.5rem 0'
+    }
+  },
+  colors: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  colorbox: {
+    height: '20%',
+    width: '25%'
+  }
+}
 
 class MiniPalette extends React.Component {
   constructor (props) {
@@ -8,23 +46,24 @@ class MiniPalette extends React.Component {
     this.handleClick = this.handleClick.bind(this)
   }
   renderColors () {
-    return this.props.palette.colors.map(color => <div className='mini-colorbox' style={{ backgroundColor: color.color }} id={`color.name-${this.props.id}`} key={color.color}>&nbsp;</div>)
+    return this.props.palette.colors.map(color => <div className={this.props.classes.colorbox} style={{ backgroundColor: color.color }} id={`color.name-${this.props.id}`} key={color.color}>&nbsp;</div>)
   }
   handleClick () {
     console.log(this.props)
     this.props.history.push(`/palette/${this.props.palette.id}`)
   }
   render () {
+    const { classes, palette } = this.props
     return (
-      <div className="MiniPalette" onClick={this.handleClick}>
-        <div className="MiniPalette-colors">{this.renderColors()}</div>
-        <div className="MiniPalette-data">
-          <h3>{this.props.palette.paletteName}</h3>
-          <span>{this.props.palette.emoji}</span>
+      <div className={classes.root} onClick={this.handleClick}>
+        <div className={classes.colors}>{this.renderColors()}</div>
+        <div className={classes.data}>
+          <h3>{palette.paletteName}</h3>
+          <span>{palette.emoji}</span>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default MiniPalette
+export default withStyles(styles)(MiniPalette)
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,21 +1,62 @@
-import './PaletteList.css'
+// import './PaletteList.css'
 import React from 'react'
 import MiniPalette from './MiniPalette'
 import seedColors from './seedColors'
+import { Link } from 'react-router-dom'
 import uuidv4 from 'uuid/v4'
+import { withStyles } from '@material-ui/styles'
+
+const styles = {
+  root: {
+    margin: '0 auto',
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: '#3f3abf'
+  },
+  container: {
+    width: '70%',
+    height: '50%',
+    margin: 'auto auto',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '1rem'
+  },
+  navbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '70%',
+    margin: 'auto',
+    '& a': {
+      alignSelf: 'center',
+      color: '#fff'
+    },
+    '& h2': {
+      color: '#fff',
+      fontFamily: 'Quattrocento Sans, sans-serif',
+      letterSpacing: '2px',
+      cursor: 'pointer'
+    }
+  }
+}
 
 class PaletteList extends React.Component {
   renderSeedColors () {
+    const { classes } = this.props;
     return seedColors.map(palette => (
-      <div className="Palette-item" key={uuidv4()} >
+      <div className={classes.item} key={uuidv4()} >
         <MiniPalette palette={palette} history={this.props.history} />
       </div>
     ));
   }
   render () {
+    const { classes } = this.props
     return (
-      <div className='PaletteList'>
-        <div className='PaletteList-container'>
+      <div className={classes.root}>
+        <div className={classes.navbar}>
+          <h2>React Colors</h2>
+          <Link to='/'>Create Palette</Link>
+        </div>
+        <div className={classes.container}>
           {this.renderSeedColors()}
         </div>
       </div>
@@ -23,4 +64,4 @@ class PaletteList extends React.Component {
   }
 }
 
-export default PaletteList
+export default withStyles(styles)(PaletteList)
